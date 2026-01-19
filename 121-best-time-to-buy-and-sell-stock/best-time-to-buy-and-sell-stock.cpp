ae64105d00b3abe7623e84562c5be6a597e78def
Ayanshi Jain <ayanshijain7@gmail.com>
2026-01-19 20:45:49 +0530

    Time: 0 ms (100.00%) | Memory: 97.2 MB (98.99%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int maxProf = 0;
+
+        for(int i = 1; i < prices.size(); i++)
+        {
+            if(prices[i] < mini)
+            {
+                mini = prices[i];
+            }
+            else
+            {
+                maxProf = max(maxProf, prices[i] - mini);
+            }
+        }
+        return maxProf;
+    }
+};
