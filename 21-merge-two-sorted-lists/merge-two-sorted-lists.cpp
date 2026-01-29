ea88085481b5c2e26c101bcdaac08345989aecfa
Ayanshi Jain <ayanshijain7@gmail.com>
2026-01-29 12:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (61.53%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,8 +11,7 @@
 class Solution {
 public:
 
-    ListNode* solve(ListNode* list1, ListNode* list2)
-    {
+    ListNode* solve(ListNode* list1, ListNode* list2){
         if(list1 -> next == NULL)
         {
             list1 -> next = list2;
@@ -20,26 +19,25 @@ public:
         }
 
         ListNode* curr1 = list1;
-        ListNode* next1 = curr1 -> next;
+        ListNode* next1 = curr1-> next;
         ListNode* curr2 = list2;
         ListNode* next2 = curr2 -> next;
 
-        while(next1 != NULL && curr2 != NULL)
+        while(curr2 != NULL && next1 != NULL)
         {
-            if((curr2 -> val >= curr1 -> val) && (curr2 -> val <= next1 -> val))
+            if(curr2 -> val >= curr1 -> val && curr2 -> val <= next1 -> val)
             {
                 curr1 -> next = curr2;
                 next2 = curr2 -> next;
                 curr2 -> next = next1;
 
                 curr1 = curr2;
-                curr2 =next2;
+                curr2 = next2;
             }
             else
             {
                 curr1 = next1;
-                next1 = next1 -> next;
-
+                next1 = next1 ->next;
                 if(next1 == NULL)
                 {
                     curr1 -> next = curr2;
@@ -49,14 +47,12 @@ public:
         }
         return list1;
     }
-
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
+
         if(list1 == NULL)
         {
             return list2;
         }
-
         if(list2 == NULL)
         {
             return list1;
@@ -64,11 +60,12 @@ public:
 
         if(list1 -> val <= list2 -> val)
         {
-            return solve(list1,list2);
+            return solve(list1, list2);
         }
         else
         {
             return solve(list2, list1);
         }
+        
     }
 };
